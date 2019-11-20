6b289d4f42dc53df8d3cfecc7949d04149748004
unknown <ctw1048@hanmail.net>
2019-11-21 02:50:26 +0900

    회원가입완료

--- src/commons/utils.js
+++ src/commons/utils.js
@@ -5,22 +5,77 @@ import config from './_config';
  * 빈객체 or 빈문자열 or null or undefined 인 경우 빈 값임
  * false or 0 인 경우는 빈값이 아님
  * @method
- * @param
- * @return {String}
+ * @param {Any} value - 검사할
+ * @return {Boolean} - 빈값 여부
 */
-// const isEmpty = (value) => {
-//     if ((value == "" || value == null || value == undefined || (value != null && typeof value == "object" && !Object.keys(value).length)) && value != 0) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// };
-const isEmpty = (value) => {
-    if (value == "" || value == null || value == undefined || ( value != null && typeof value == "object" && !Object.keys(value).length)) {
+let isEmpty = (value) => {
+    if (
+        value === "" 
+        || value === null 
+        || value === undefined 
+        || ( value !== null && typeof value === "object" && !Object.keys(value).length)
+    ) {
         return true;
     } else {
         return false;
     }
+};
+
+/** 
+ * 타입체크
+ * @method
+ * @param {Any} input - 체크할 대상
+ * @param {String} type - 체크할 타입
+ * @return {Boolean} 타입일치 여부
+*/
+const checkType = (input, type) => {
+    //인자가 부족할 경우 반환
+    if(isEmpty(input) || isEmpty(type)){
+        throw Error("checkType 인자가 부족합니다.");
+    }
+    const typeList = ["number", "string", "object", "function", "boolean", "undefined"];
+    let typeFlag = false;
+
+    //두번째 인자 검사
+    for(v of typeList){
+        if(v === type){
+            typeFlag = true;
+        }
+    }
+
+    //두번째 인자 유효하지 않은 경우
+    if(!typeFlag){
+        throw Error(type + " 은 정상적인 타입이 아닙니다.");
+    }
+
+    //실제 타입검사
+    if(typeof input === type){
+        return true;
+    } else {
+        return false;
+        // throw Error(input + " 의 타입이 " + type + " 이 아닙니다.");
+    }
+};
+
+/** 
+ * Object의 각 프로퍼티들이 Json String 형태인 경우 JSON.parse 수행하여 반환
+ * @method
+ * @param {Object} obj - json으로 파싱할 대상 Object
+ * @return {Object} - 파싱 완료한 Object
+*/
+function checkAndParseJson(obj) {
+    for (v in obj) {
+        let checkJson = false;
+        try {
+            checkJson = JSON.parse(obj[v]);
+        } catch (e) {
+            // console.log("프로퍼티는 JSON String 이 아닙니다.");
+        }
+        if (!!checkJson && typeof checkJson == "object") {
+            obj[v] = JSON.parse(obj[v]);
+        }
+    }
+    return obj;
 };
 
 /** 
@@ -60,7 +115,7 @@ const doFetch = async (obj) => {
 }
 
 /** 
- * yyyyMMddhhssmm 를 yyyy-MM-dd hh:mm 형식으로 반환
+ * yyyyMMddhhssmm 를 yyyy-MM-dd hh:ss:mm 형식으로 반환
  * @method
  * @param {String} dateStr - 문자열로 된 시각
  * @return {String} - 포멧화된 시각
@@ -72,7 +127,7 @@ const getTimeFormat = (dateStr) => {
     let hour = dateStr.substring(8,10);
     let minute = dateStr.substring(10,12);
     let second = dateStr.substring(12,14);
-    let res = `${year}-${month}-${day} ${hour}:${minute}`;
+    let res = `${year}-${month}-${day} ${hour}:${minute}:${second}`;
     return res;
 }
 
@@ -183,13 +238,149 @@ const getPastDate = ({y=0, m=0, d=0} = {}) => {
     return (year + "" + month + "" + day);
 }
 
+/** 
+ * 숫자여부 체크(숫자형태 문자열, 띄어쓰기 불가능)
+ * @method
+ * @param {String} str - 체크할 문자열
+ * @return {Boolean} - 숫자여부
+*/
+const isNumber = (str) => {
+   const regExp = /^[0-9]+$/;
+   return regExp.test(str);
+}
+
+/** 
+ * 한글여부 체크(ㄱㄴㄷ... 형식으로는 입력 불가능)
+ * @method
+ * @param {String} str - 체크할 문자열
+ * @param {String} type - 0 또는 입력안할시: 띄어쓰기 불가능, 1: 띄어쓰기 가능
+ * @return {Boolean} - 한글여부
+*/
+const isKorean = (str, type) => {
+    let regExp = null;
+    if(isEmpty(type) || type == "0"){
+        regExp = /^[가-힣]+$/;
+    } else if (type == "1") {
+        regExp = /^[가-힣\s]+$/;
+    }
+    return regExp.test(str);
+}
+
+/** 
+ * 영문여부 체크
+ * @method
+ * @param {String} str - 체크할 문자열
+ * @param {String} type - 0 또는 입력안할시: 띄어쓰기 불가능, 1: 띄어쓰기 가능
+ * @return {Boolean} - 영문여부
+*/
+const isEnglish = (str, type) => {
+    let regExp = null;
+    if(isEmpty(type) || type == "0"){
+        regExp = /^[a-zA-Z]+$/;
+    } else if (type == "1") {
+        regExp = /^[a-zA-Z\s]+$/;
+    }
+    return regExp.test(str);
+}
+
+/** 
+ * 한글 또는 영문여부 체크
+ * @method
+ * @param {String} str - 체크할 문자열
+ * @param {String} type - 0 또는 입력안할시: 띄어쓰기 불가능, 1: 띄어쓰기 가능
+ * @return {Boolean} - 한글 또는 영문여부
+*/
+const isKorOrEng = (str, type) => {
+    let regExp = null;
+    if(isEmpty(type) || type == "0"){
+        regExp = /^[가-힣a-zA-Z]+$/;
+    } else if (type == "1") {
+        regExp = /^[가-힣a-zA-Z\s]+$/;
+    }
+    return regExp.test(str);
+}
+
+/** 
+ * 이메일형식 여부 체크
+ * @method
+ * @param {String} str - 체크할 문자열
+ * @return {Boolean} - 이메일형식 여부
+*/
+const isEmail = (str) => {
+    let regExp = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+    return regExp.test(str);
+}
+
+/** 
+ * 전화번호형식 여부 체크(2~3자리-3~4자리-4자리)
+ * @method
+ * @param {String} str - 체크할 문자열
+ * @return {Boolean} - 전화번호형식 여부
+*/
+const isPhone = (str) => {
+    let regExp = /^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$/;
+    return regExp.test(str);
+}
+
+/** 
+ * 아이디형식 여부 체크(영문자로 시작하는 6~20자 영문자 또는 숫자)
+ * @method
+ * @param {String} str - 체크할 문자열
+ * @return {Boolean} - 아이디형식 여부
+*/
+const isId = (str) => {
+    let regExp = /^[a-z]+[a-z0-9]{5,19}$/g;
+    return regExp.test(str);
+}
+
+/** 
+ * 비밀번호형식 여부 체크(최소 8 자 및 최대 50 자, 대문자 하나 이상, 소문자 하나 이상, 숫자 하나 이상, 특수 문자 하나 이상)
+ * @method
+ * @param {String} str - 체크할 문자열
+ * @return {Boolean} - 이메일형식 여부
+*/
+const isPassword = (str) => {
+    let regExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~`!@#$%\^&*()-+=?])[A-Za-z\d~`!@#$%\^&*()-+=?]{8,50}/;
+    return regExp.test(str);
+}
+
+/** 
+ * 도메인여부 체크
+ * @method
+ * @param {String} str - 체크할 문자열
+ * @param {String} type - 0 또는 입력안할시: http:// https:// 포함안해도 되고 해도 되고, 1: http:// https:// 꼭 포함, 2: http:// https:// 포함하면 안됨
+ * @return {Boolean} - 영문여부
+*/
+const isDomain = (str, type) => {
+    let regExp = null;
+    if(isEmpty(type) || type == "0"){
+        regExp = /^(((http(s?))\:\/\/)?)([0-9a-zA-Z\-]+\.)+[a-zA-Z]{2,6}(\:[0-9]+)?(\/\S*)?$/;
+    } else if (type == "1") {
+        regExp = /^((http(s?))\:\/\/)([0-9a-zA-Z\-]+\.)+[a-zA-Z]{2,6}(\:[0-9]+)?(\/\S*)?$/;
+    } else if (type == "2") {
+        regExp = /^[^((http(s?))\:\/\/)]([0-9a-zA-Z\-]+\.)+[a-zA-Z]{2,6}(\:[0-9]+)?(\/\S*)?$/;
+    }
+    return regExp.test(str);
+}
+
 export default {
     isEmpty,
+    checkType,
+    checkAndParseJson,
     getApiServer,
     doFetch,
     getTimeFormat,
     postPicture,
     postDocument,
     getToday,
-    getPastDate
+    getPastDate,
+    isNumber,
+    isKorean,
+    isEnglish,
+    isKorOrEng,
+    isEmail,
+    isPhone,
+    isId,
+    isPassword,
+    isDomain
 }
--- src/components/Join.js
+++ src/components/Join.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, TextInput, Image, Button, Picker, TouchableHighlight, KeyboardAvoidingView } from 'react-native';
+import { Text, View, TextInput, Image, Button, Picker, TouchableHighlight, KeyboardAvoidingView, Alert } from 'react-native';
 import { useQuery, useMutation, useLazyQuery } from '@apollo/react-hooks';
 import { connect } from 'react-redux';
 import DatePicker from 'react-native-datepicker'
@@ -24,7 +24,16 @@ const Join = (props) => {
   const [ site, setSite ] = useState(""); // 사이트
   const [ introduction, setIntroduction ] = useState(""); // 소개글
 
-  const [join, { loading, error }] = useMutation(JOIN_PAGE_JOIN);
+  const [join, { data, loading, error, called }] = useMutation(JOIN_PAGE_JOIN, {onCompleted: (data) => {onJoinComplete(data);}});
+  // const [join, { data, loading, error, called }] = useMutation(JOIN_PAGE_JOIN);
+
+  let idElem = null; // 아이디입력란
+  let passwordElem = null; // 패스워드입력란
+  let passwordElem2 = null; // 패스워드입력란2
+  let nameElem = null; // 이름입력란
+  let birthElem = null; // 생년월일입력란
+  let siteElem = null; // 사이트입력란
+  let introductionElem = null; // 소개글입력란
 
   useEffect(() => {
     console.log("Join 마운트!");
@@ -33,19 +42,88 @@ const Join = (props) => {
     };
   });
 
+  const onJoinComplete = (data) => {
+    if(data && data.insertMember){
+      // 성공인 경우
+      Alert.alert("회원가입에 성공", "로그인페이지로 이동합니다.", 
+        [
+          {text: "네", onPress: () => {props.navigation.navigate("Login");}}
+        ], 
+        {
+          onDismiss: () => {props.navigation.navigate("Login");}
+        }
+      );
+    } else if (data && !data.insertMember) {
+      // 실패인 경우
+      Alert.alert(
+        "회원가입 실패", 
+        "중복된 아이디입니다.", 
+        [
+          {text: "네"}
+        ]
+      );
+    }
+  }
+
   // 입력값 유효성검사
   const checkValidData = () => {
-    // 필수값검사
-    // 이메일 등 형식검사
+    // 아이디 체크
+    if(!utils.isId(id)){
+      Alert.alert("", "아이디 영문자로 시작하는 6~20자 영문자 또는 숫자", [{text: "네"}]);
+      idElem.focus();
+      return false;
+      // Alert.alert('제목', '아이디를 입력하세요.', [
+      //   {text: '아니오', onPress: () => console.log('아니오 클릭!')},
+      //   {text: '네', onPress: () => console.log('네 클릭!')},
+      // ], {
+      //   // cancelable: false,
+      //   onDismiss: () => {
+      //     console.log('취소됨...');
+      //     idElem.focus();
+      //   }
+      // }
+      // );
+    }
+    // 비밀번호 체크
+    if(!utils.isPassword(password)){
+      Alert.alert("", "비밀번호 최소 8 자 및 최대 50 자, 대문자 하나 이상, 소문자 하나 이상, 숫자 하나 이상, 특수 문자 하나 이상", [{text: "네"}]);
+      passwordElem.focus();
+      return false;
+    }
+    // 비밀번호2 체크
+    if(password !== password2){
+      Alert.alert("", "비밀번호가 일치하지 않습니다.", [{text: "네"}]);
+      passwordElem2.focus();
+      return false;
+    }
+    // 이름 체크
+    if(utils.isEmpty(name)){
+      Alert.alert("", "이름을 입력하세요.", [{text: "네"}]);
+      nameElem.focus();
+      return false;
+    }
+    // 사이트주소 체크(입력된 경우에만)
+    if(!utils.isEmpty(site)){
+      // 도메인형식 체크
+      if(!utils.isDomain(site, "1")){
+        Alert.alert("", "사이트 주소가 올바르지 않습니다.", [{text: "네"}]);
+        siteElem.focus();
+        return false;
+      }
+    }
+    return true;
   }
 
   // 회원가입 진행
   const triggerJoin = () => {
+    let isValid = checkValidData();
+    if(!isValid){
+      return false;
+    }
     const data = {
       id, password, name, birth, gender, site, introduction
     }
     join({ variables: data });
-    // 회원가입 결과처리
   }
 
   // 생년월일 선택 범위는 오늘 날짜로부터 100년 전 날짜 ~ 오늘 날짜로 한다.
@@ -57,10 +135,10 @@ const Join = (props) => {
         <Text style={styles.fontSize20}>친구들의 사진과 동영상을 보려면 가입하세요.</Text>
       </View>
 
-      <TextInput style={styles.textInputWide} value={id} placeholder="아이디(필수)" onChangeText={setId} autoFocus={true}></TextInput>
-      <TextInput style={styles.textInputWide} value={password} placeholder="비밀번호(필수)" onChangeText={setPassword} secureTextEntry={true} textContentType="password"></TextInput>
-      <TextInput style={styles.textInputWide} value={password2} placeholder="비밀번호확인(필수)" onChangeText={setPassword2} secureTextEntry={true} textContentType="newPassword"></TextInput>
-      <TextInput style={styles.textInputWide} value={name} placeholder="성명(필수)" onChangeText={setName}></TextInput>
+      <TextInput style={styles.textInputWide} value={id} placeholder="아이디(필수)" onChangeText={setId} autoCapitalize={"none"} ref={(elem) => {idElem = elem}}></TextInput>
+      <TextInput style={styles.textInputWide} value={password} placeholder="비밀번호(필수)" onChangeText={setPassword} secureTextEntry={true} textContentType="password" autoCapitalize={"none"} ref={(elem) => {passwordElem = elem}}></TextInput>
+      <TextInput style={styles.textInputWide} value={password2} placeholder="비밀번호확인(필수)" onChangeText={setPassword2} secureTextEntry={true} textContentType="newPassword" autoCapitalize={"none"} ref={(elem) => {passwordElem2 = elem}}></TextInput>
+      <TextInput style={styles.textInputWide} value={name} placeholder="성명(필수)" onChangeText={setName} ref={(elem) => {nameElem = elem}}></TextInput>
 
       <DatePicker
         style={styles.textInputWide}
@@ -98,7 +176,7 @@ const Join = (props) => {
         <Picker.Item label="여성" value="F" />
       </Picker>
 
-      <TextInput style={styles.textInputWide} value={site} placeholder="사이트주소" onChangeText={setSite} textContentType="URL"></TextInput>
+      <TextInput style={styles.textInputWide} value={site} placeholder="사이트주소" onChangeText={setSite} textContentType="URL" autoCapitalize={"none"} ref={(elem) => {siteElem = elem}}></TextInput>
       <TextInput style={styles.textInputWide} value={introduction} placeholder="자기소개" onChangeText={setIntroduction} multiline = {true} numberOfLines = {10}></TextInput>
 
       <TouchableHighlight style={styles.button}>
